e0e6fc9eb38f72edfe295c9eb913393265131230
Mojito <enzox9191@gmail.com>
2024-04-02 15:16:14 +0200

    Initial Base

---
+++ src/vue/TableauStat3.java
@@ -0,0 +1,34 @@
+package vue;
+
+import javax.swing.table.AbstractTableModel;
+
+public class TableauStat3 extends AbstractTableModel{
+	private static final long serialVersionUID = 1L;
+	//TODO Créer les attributs
+	
+	//TODO Créer le constructeur
+	
+	@Override
+	public int getColumnCount() {
+		// TODO Compléter getColumnCount
+		return 0;
+	}
+	@Override
+	public String getColumnName(int columnIndex) {
+		// TODO Compléter getColumnName
+		return "";
+	}
+
+	@Override
+	public int getRowCount() {
+		// TODO Compléter getRowCount
+		return 0;
+	}
+	
+
+	@Override
+	public Object getValueAt(int rowIndex, int columnIndex) {
+		// TODO Compléter getValueAt
+		return null;
+	}
+}
