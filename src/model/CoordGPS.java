c77e35d58d9c256e4fb12cb6097e15f28fed1b04
.Mojito <enzox9191@gmail.com>
2024-04-16 14:40:23 +0200

    up

--- src/model/CoordGPS.java
+++ src/model/CoordGPS.java
@@ -11,7 +11,20 @@ public class CoordGPS {
 		latitude = lat;
 		longitude = lon;
 	}
+
+	public static int calculateDistance(double startLatitude, double startLongitude, double endLatitude, double endLongitude) 
+	{
+		double startLatitudeRad = Math.toRadians(48.853);
+		double startLongitudeRad = Math.toRadians(2.35);
+		double endLatitudeRad = Math.toRadians(endLatitude);
+		double endLongitudeRad = Math.toRadians(endLongitude);
 	
+		double x = (endLongitudeRad - startLongitudeRad) * Math.cos((startLatitudeRad + endLatitudeRad) / 2);
+		double y = (endLatitudeRad - startLatitudeRad);
+		
+		return (int)Math.sqrt(x * x + y * y) * 6371;
+	}
+ 	
 	private static String[] extractLatLongFromGeometry(String geometry) {
 		String extract = geometry.substring(2, geometry.length()-1);
 		String[] coord = extract.split(", ");
